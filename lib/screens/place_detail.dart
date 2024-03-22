b8f7729bb9dfb69af0750f69402e9e387b728755
never47 <guseinovi2002@gmail.com>
2024-03-22 12:11:56 +0100

    add: designed place_detail screen

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -6,6 +6,13 @@ class PlacesDetail extends StatelessWidget {
 
   final Place place;
 
+  String get locationImage {
+    final lon = place.placeLocation.longitude;
+    final lat = place.placeLocation.latitude;
+
+    return 'https://maps.geoapify.com/v1/staticmap?style=osm-carto&width=600&height=300&center=lonlat:$lon,$lat&zoom=19&apiKey=e8fad2e9672643bf8a68bd94a243454d';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +26,45 @@ class PlacesDetail extends StatelessWidget {
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(
+                    locationImage,
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 16,
+                  ),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        Colors.black54,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Text(
+                    place.placeLocation.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
